ae7bcecf89fb4d115c03c1b011a5ce7a6b2558de
Mehedi Hasan <mhasan502@gmail.com>
2023-03-21 15:22:29 +0600

    added font

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import '@mantine/core/styles.css';
-import {MantineProvider} from '@mantine/core';
+import {createTheme, MantineProvider} from '@mantine/core';
+
+const poppinsFont = createTheme({
+    fontFamily: 'Open Sans, sans-serif',
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={poppinsFont}>
       <App/>
     </MantineProvider>
   </React.StrictMode>,
