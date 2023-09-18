773a3b4255f9795362f4d942dfda6238179313aa
mhasan502 <mhasan502@gmail.com>
2023-09-19 00:24:37 +0600

    removed activeindex and fixed index logo issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,93 +1,76 @@
 import {Link} from 'react-router-dom';
 import {AppShell, Burger, Group, Box, Button, Image, Title} from '@mantine/core';
 import PropTypes from 'prop-types';
-import {useState} from "react";
 
 const buttonData = [
-	{tag: 'Experience', link: '/experience/'},
-	{tag: 'Education', link: '/education/'},
-	{tag: 'Projects', link: '/projects/'},
-	{tag: 'Skills', link: '/skills/'},
-	{tag: 'Publications', link: '/publications/'},
+  {tag: 'Experience', link: '/experience/'},
+  {tag: 'Education', link: '/education/'},
+  {tag: 'Projects', link: '/projects/'},
+  {tag: 'Skills', link: '/skills/'},
+  {tag: 'Publications', link: '/publications/'},
 ];
 
 const NavbarOptions = () => {
-	const [activeButtonIndex, setActiveButtonIndex] = useState(null);
+  const renderButton = (item) => (
+    <Link to={item.link} key={item.tag}>
+      <Button size={'compact-md'} p={2} variant={'subtle'}>
+        {item.tag}
+      </Button>
+    </Link>
+  );
 
-	const renderButton = (item, index) => (
-		<Link
-			to={item.link}
-			key={item.tag}
-		>
-			<Button size={'compact-md'} p={0}
-				variant={index === activeButtonIndex ? 'primary' : 'subtle'}
-				onClick={() => setActiveButtonIndex(index)}
-			>
-				{item.tag}
-			</Button>
-		</Link>
-	);
-
-	return (
-		<>
-			{buttonData.map(renderButton)}
-		</>
-	);
+  return (
+    <>
+      {buttonData.map(renderButton)}
+    </>
+  );
 }
 const DesktopNavbarOptions = () => {
-	return (
-		<Group gap='xs' visibleFrom='sm' justify='center'>
-			<NavbarOptions/>
-		</Group>
-	);
+  return (
+    <Group gap='xs' visibleFrom='sm' justify='center'>
+      <NavbarOptions/>
+    </Group>
+  );
 }
 
 // TODO - button position center
 const MobileNavbarOptions = () => {
-	return (
-		<AppShell.Navbar py='md' px={4}>
-			<NavbarOptions/>
-		</AppShell.Navbar>
-	);
+  return (
+    <AppShell.Navbar py='md' px={4}>
+      <NavbarOptions/>
+    </AppShell.Navbar>
+  );
 }
 
 const Header = ({opened, toggle}) => {
-	return (
-		<>
-			<AppShell.Header color='#000000'>
-				<Box p='sm' styles={[]}>
-					<Group h='100%' px='sm'>
-						<Burger opened={opened} onClick={toggle} hiddenFrom='sm' size='sm'/>
-						<Group justify='space-between' style={{flex: 1}}>
-							<Link to={'/'} style={{ textDecoration: 'none' }}>
-								<Group>
-								<Image
-									h={{base: 35, md: 45, lg: 55}} radius={'md'}
-									src={'../../public/Mehedi.jpg'}
-								/>
-									<Title
-										order={3}
-										tt="uppercase"
-										c={'blue'}
-										style={{ fontFamily: 'Chakra Petch'}}
-									>
-										Md. Mehedi Hasan
-									</Title>
-								</Group>
-							</Link>
-							<DesktopNavbarOptions/>
-						</Group>
-					</Group>
-				</Box>
-			</AppShell.Header>
-			<MobileNavbarOptions/>
-		</>
-	);
+  return (
+    <>
+      <AppShell.Header color='#000000'>
+        <Box p='sm' styles={[]}>
+          <Group h='100%' px='sm'>
+            <Burger opened={opened} onClick={toggle} hiddenFrom='sm' size='sm'/>
+            <Group justify='space-between' style={{flex: 1}}>
+              <Link to={'/'} style={{textDecoration: 'none'}}>
+                <Group>
+                  <Image h={{base: 35, md: 45, lg: 55}} radius={'md'} src={'../../public/Mehedi.jpg'}/>
+                  <Title order={3} tt="uppercase" c={'blue'} style={{fontFamily: 'Chakra Petch'}}>
+                    Md. Mehedi Hasan
+                  </Title>
+                </Group>
+              </Link>
+              <DesktopNavbarOptions/>
+            </Group>
+          </Group>
+        </Box>
+      </AppShell.Header>
+      <MobileNavbarOptions/>
+    </>
+  );
 }
 
 Header.propTypes = {
-	opened: PropTypes.bool.isRequired,
-	toggle: PropTypes.func.isRequired,
+  opened: PropTypes.bool.isRequired,
+  toggle: PropTypes.func.isRequired,
 };
 
 export default Header;
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -26,20 +26,20 @@ const Index = () => {
 
       <Container size={'xs'}>
         <Group justify="center">
-          <Anchor c={'#2c323b'} px={10} href={'https://github.com/mhasan502'} target={'_blank'}>
-            <FaGithub size={40}/>
+          <Anchor c={'#2c323b'} px={4} href={'https://github.com/mhasan502'} target={'_blank'}>
+            <FaGithub size={36}/>
           </Anchor>
-          <Anchor c={'#0A65C0'} px={10} href={'https://linkedin.com/in/Mhasan502'} target={'_blank'}>
-            <FaLinkedin size={40}/>
+          <Anchor c={'#0A65C0'} px={4} href={'https://linkedin.com/in/Mhasan502'} target={'_blank'}>
+            <FaLinkedin size={36}/>
           </Anchor>
-          <Anchor c={'#2364d0'} px={10} href={'https://scholar.google.com/citations?user=1E7N-BoAAAAJ'} target={'_blank'}>
-            <SiGooglescholar size={40}/>
+          <Anchor c={'#2364d0'} px={4} href={'https://scholar.google.com/citations?user=1E7N-BoAAAAJ'} target={'_blank'}>
+            <SiGooglescholar size={36}/>
           </Anchor>
-          <Anchor c={'#f48024'} px={10} href={'https://stackoverflow.com/users/14150890/mhasan502'} target={'_blank'}>
-            <FaStackOverflow size={40}/>
+          <Anchor c={'#f48024'} px={4} href={'https://stackoverflow.com/users/14150890/mhasan502'} target={'_blank'}>
+            <FaStackOverflow size={36}/>
           </Anchor>
-          <Anchor c={'#030b67'} px={10} href={'steamcommunity.com/id/Mhasan502'} target={'_blank'}>
-            <FaSteam size={40}/>
+          <Anchor c={'#030b67'} px={4} href={'steamcommunity.com/id/Mhasan502'} target={'_blank'}>
+            <FaSteam size={36}/>
           </Anchor>
         </Group>
       </Container>
